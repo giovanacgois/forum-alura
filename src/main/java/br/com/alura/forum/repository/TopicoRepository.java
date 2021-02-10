bd025d354e6a0ee4177fbf2ee299f8be85a5f971
Giovana <giovana@maptriz.com.br>
2021-02-10 09:18:55 -0300

    Criação api

---
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -0,0 +1,18 @@
+package br.com.alura.forum.repository;
+
+import br.com.alura.forum.modelo.Topico;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+    //Se tiver o atributo cursoNome na classe Topico, basta mudar o nome do método para finByCurso_Nome,
+    // que o Spring entende que se trata de um relacionamento.
+    List<Topico> findByCursoNome(String nomeCurso);
+
+    @Query("SELECT t from Topico t WHERE t.curso.nome = :nomeCurso")
+    List<Topico> carregarPorNomeDoCurso(@Param("nomeCurso") String nomeCurso);
+}
