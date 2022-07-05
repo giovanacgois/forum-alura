a86afb62e8ce4f37e4cedc5e1c03b826d616ef5a
giovanacgois <giovanacgois@gmail.com>
2022-07-04 23:09:20 -0300

    Implementando paginação

--- src/main/java/br/com/alura/forum/controller/dto/TopicoDTO.java
+++ src/main/java/br/com/alura/forum/controller/dto/TopicoDTO.java
@@ -1,10 +1,9 @@
 package br.com.alura.forum.controller.dto;
 
 import br.com.alura.forum.modelo.Topico;
+import org.springframework.data.domain.Page;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class TopicoDTO {
     private Long id;
@@ -36,10 +35,8 @@ public class TopicoDTO {
         return dataCriacao;
     }
 
-    public static List<TopicoDTO> converter(List<Topico> topicos) {
-        return topicos.stream().map(TopicoDTO::new).collect(Collectors.toList());
+    public static Page<TopicoDTO> converter(Page<Topico> topicos) {
+        return topicos.map(TopicoDTO::new);
     }
 
-    public static class TopicoDto {
-    }
 }
